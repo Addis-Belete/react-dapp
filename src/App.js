4539412fa6f4be347fbd05f787c9972331445294
Addis Belete <addis.bel@gmail.com>
2022-02-15 09:10:32 +0300

    Add a method to ask to connect users Meta Mask

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+
+const greeterAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [greeting, setGreetingValue] = useState()
+	// request access to users MetaMask account
+	async function requestAccount() {
+		await window.ethereum.request({ method: 'eth_requestAccounts' })
+	}
+
+	return (
+		<div className="App">
+			Hello
+		</div>
+	);
 }
 
 export default App;
