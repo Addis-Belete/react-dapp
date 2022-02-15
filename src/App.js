f94255999c0adc951784f73e0ba3359aac0fee99
Addis Belete <addis.bel@gmail.com>
2022-02-15 09:20:25 +0300

    Add a method to fetch greeting from the blockchain

--- src/App.js
+++ src/App.js
@@ -13,6 +13,22 @@ function App() {
 		await window.ethereum.request({ method: 'eth_requestAccounts' })
 	}
 
+	//call the smart contract, read the current reading value
+
+	async function fetchGreeting() {
+		if (typeof window.ethereum !== 'undefined') {
+			const provider = new ethers.providers.Web3Provider(window.ethereum)
+			const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+			try {
+				const data = await contract.great()
+				console.log('data: ', data)
+			} catch (err) {
+				console.log("Error: ", err)
+			}
+		}
+
+	}
+
 	return (
 		<div className="App">
 			Hello
