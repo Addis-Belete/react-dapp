fc307e581e723b6c0b3efd66cfa59d740182cce7
Addis Belete <addis.bel@gmail.com>
2022-02-16 10:31:54 +0300

    Add a transfer function to Token contract

---
+++ contracts/Token.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.6.5;
+
+import "hardhat/console.sol";
+
+contract Token {
+    string public name = "Addis Token";
+    string public symbol = "ADD";
+    uint256 public totalSupply = 1000000;
+    mapping(address => uint256) balances;
+
+    constructor() public {
+        balances[msg.sender] = totalSupply;
+    }
+
+    function transfer(address to, uint256 amount) external {
+        require(balances[msg.sender] >= amount, "Not enough tokens");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+}
