39148190df9325a59c091ed2e5e8a884ce9e1b40
Addis Belete <addis.bel@gmail.com>
2022-02-17 16:37:01 +0300

    Add a time out function

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-
-
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
-
 import Token from './artifacts/contracts/Token.sol/Token.json'
 
 const tokenAddress = "0x07E85EA990d273eA5d18192a403Be08fd13FAA38"
 function App() {
-
-
-
 	const [amount, setAmount] = useState(0);
 	const [userAccount, setUserAccount] = useState('')
-	const [balance, setBalance] = useState('0')
+	const [balance, setBalance] = useState('')
+	const [message, setMessage] = useState(false);
 	// request access to users MetaMask account
 	async function requestAccount() {
 		await window.ethereum.request({ method: 'eth_requestAccounts' })
 	}
-
-	//call the smart contract, read the current reading value
-
-
-	// the smart contract, send an update
-
-
 	async function fetchTransfer() {
 		if (typeof window.ethereum !== 'undefined') {
 			await requestAccount()
@@ -34,6 +22,8 @@ function App() {
 			const transation = await contract.transfer(userAccount, amount);
 			try {
 				await transation.wait();
+				setMessage(true)
+
 				console.log(`${amount} Coins successfully sent to ${userAccount}`);
 			} catch (err) {
 				console.log("Error: ", err)
@@ -41,7 +31,6 @@ function App() {
 		}
 	}
 	//getBalance function
-
 	async function getBalance() {
 		if (typeof window.ethereum !== 'undefined') {
 			const [account] = await window.ethereum.request({ method: "eth_requestAccounts" })
@@ -60,20 +49,35 @@ function App() {
 
 		}
 	}
+	useEffect(() => {
+		setTimeout(() => {
+			if (message === true) {
+				setMessage(false)
+				setAmount(0)
+				setUserAccount('')
+
+			}
+		}, 5000)
+
+	}, [message])
+
+
 	return (
-		<div className="App" style={{ display: "flex", justifyContent: "space-evenly" }}>
+		<div className="w-1/4 mx-auto mt-40 border shadow-lg  p-4" style={{ display: "flex", justifyContent: "space-evenly" }}>
 
 			<div style={{ marginTop: 30 }}>
-				<h1>My First Full-Stack Dapp</h1>
-				<label style={{ marginRight: "10px" }} htmlFor="address" >Address</label>
-				<input type="text" id="address" onChange={((e) => { setUserAccount(e.target.value) })} value={userAccount} placeholder='Enter Address' style={{ width: 200, padding: 5, marginBottom: 20 }} />
+				<h1 className='uppercase font-bold mb-5 text-center text-lg rounded-lg'>My First Full-Stack Dapp</h1>
+				<p className={`${message ? "block" : "hidden"} text-xs text-green-700`} >{`${amount} Coins successfully sent to ${userAccount}`}</p>
+				<label className='font-bold' style={{ marginRight: "10px" }} htmlFor="address" >Account</label>
+				<input className='border ' type="text" id="address" onChange={((e) => { setUserAccount(e.target.value) })} value={userAccount} placeholder='Enter Your Account Address' style={{ width: 250, padding: 5, marginBottom: 20 }} />
 				<br />
-				<label htmlFor="amount" style={{ marginRight: "10px" }} >Amount</label>
-				<input type="number" id="amount" onChange={((e) => setAmount(e.target.value))} value={amount} placeholder='Enter Amount' style={{ width: 200, padding: 5, marginBottom: 20 }} />
+				<label className='font-bold' htmlFor="amount" style={{ marginRight: "10px" }} >Amount</label>
+				<input className='border' type="number" id="amount" onChange={((e) => setAmount(e.target.value))} value={amount} placeholder='Enter Amount' style={{ width: 250, padding: 5, marginBottom: 20 }} />
 				<br />
-				<button onClick={fetchTransfer} style={{ padding: "5px 10px", marginBottom: 20 }}>Transfer</button><br />
-				<button onClick={getBalance} style={{ padding: "5px 10px" }}>Get Balance</button>
-				<span style={{ marginLeft: 35 }}>{`Your Balance is:-   ${balance}`}</span>
+				<button className='border ' onClick={fetchTransfer} style={{ padding: "5px 10px", marginBottom: 20, marginLeft: "21%" }}>Transfer</button><br />
+				<button className='border mb-10' onClick={getBalance} style={{ padding: "5px 10px", marginLeft: "21%" }}>Get Balance</button>
+				<span className='text-green-700' style={{ marginLeft: "20px" }}>{`  ${balance}`}</span>
+
 			</div>
 		</div>
 	);
